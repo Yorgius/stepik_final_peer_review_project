de60f39cffa242542a15bae48ff7a2542c987d99
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-23 08:37:56 +0300

    added main page testing

---
+++ conftest.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import  pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--lang',
+        action='store',
+        default='ru',
+        help='Please choose language settings: en, ru, fr or another'
+    )
+
+@pytest.fixture
+def browser(request) -> object:
+    lang: str = request.config.getoption('lang')
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
---
+++ pages/base_page.py
@@ -0,0 +1,42 @@
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, browser, url, timeout=10) -> None:
+        self.browser = browser
+        self.browser.implicitly_wait = timeout
+        self.url = url
+
+    def open(self) -> None:
+        self.browser.get(self.url)
+
+    def is_element_present(self, condition_search, selector_search) -> bool:
+        try:
+            self.browser.find_element(condition_search, selector_search)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def is_not_element_present(self, condition_search, selector_search, timeout=4) -> bool:
+        try:
+            WebDriverWait(self.browser, timeout).\
+                until(EC.presence_of_element_located((condition_search, selector_search)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_not_disappeared(self, condition_search, selector_search, timeout=4) -> bool:
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((condition_search, selector_search)))
+        except TimeoutException:
+            return False
+        return True
+
+    def get_text(self, condition_search, selector_search) -> str:
+        try:
+            element = self.browser.find_element(condition_search, selector_search)
+            return element.text
+        except NoSuchElementException:
+            return ''
