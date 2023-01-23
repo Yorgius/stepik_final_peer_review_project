72520c0a29b3e45d0134d4b1b15bdd7ea2b255e4
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-23 08:38:22 +0300

    added login page class

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+            assert self.browser.current_url.find('login'), 'It is not the login url!'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), 'login form is not present'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), 'register form is not present'
