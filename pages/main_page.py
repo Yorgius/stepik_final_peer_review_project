aee2ab11b30cefb42878f4ac3a8b8367f2658a11
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-24 08:05:04 +0300

    added testing methods for basket page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 from .locators import MainPageLocators
 from .base_page import BasePage
@@ -8,8 +8,15 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def go_to_login_page(self):
+    def go_to_login_page(self) -> None:
         login_link =  self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+
+    def should_be_basket_link(self):
+        assert self.is_element_present(*MainPageLocators.BASKET_LINK), 'This page does not contain basket link'
+
+    def go_to_basket_page(self) -> None:
+        basket_link = self.browser.find_element(*MainPageLocators.BASKET_LINK)
+        basket_link.click()
 
     
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,34 @@
+import pytest
+
+import time
+
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 MAIN_PAGE_URL = "http://selenium1py.pythonanywhere.com/"
 
+@pytest.mark.main_to_login
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, MAIN_PAGE_URL)
+    main_page = MainPage(browser, MAIN_PAGE_URL)
 
-    page.open()
-    page.should_be_login_link()
-    page.go_to_login_page()
+    main_page.open()
+    main_page.should_be_login_link()
+    main_page.go_to_login_page()
 
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+@pytest.mark.main_to_basket
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    main_page = MainPage(browser, MAIN_PAGE_URL)
+    
+    main_page.open()
+    main_page.should_be_basket_link()
+    main_page.go_to_basket_page()
+
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_page()
+    basket_page.no_items_in_the_basket()
+    basket_page.is_basket_empty()
