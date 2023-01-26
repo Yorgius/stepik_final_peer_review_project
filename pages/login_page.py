41ec8ab2304a129f8748f5eb590211f333eaae29
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-26 08:14:42 +0300

    added registration method from login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,6 +5,7 @@ from .base_page import BasePage
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -12,7 +13,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-            assert self.browser.current_url.find('login'), 'It is not the login url!'
+        assert self.browser.current_url.find('login'), 'It is not the login url!'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
@@ -21,3 +22,17 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), 'register form is not present'
+
+    # регистрация пользователя
+    def register_new_user(self, email, psw):
+        email_input = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_EMAIL_INPUT)
+        email_input.send_keys(email)
+
+        psw_input_1 = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PSW_IMPUT_1)
+        psw_input_1.send_keys(psw)
+
+        psw_input_2 = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PSW_IMPUT_2)
+        psw_input_2.send_keys(psw)
+
+        registration_submit = self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT)
+        registration_submit.click()
