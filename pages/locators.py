49e0a631ec2096b0f573c2ca007f3e546bac11d5
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-26 08:10:06 +0300

    corrected and added new locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,15 +1,33 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
 class MainPageLocators:
     LOGIN_LINK = (By.ID, "login_link")
     BASKET_LINK = (By.XPATH, "//a[text()='Посмотреть корзину']")
+    DELETE_USER_SUCCESS_MESSAGE = (By.CLASS_NAME, "alertinner")
 
 
 class LoginPageLocators:
-    # URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+    REGISTER_FORM_EMAIL_INPUT = (By.ID, "id_registration-email")
+    REGISTER_FORM_PSW_IMPUT_1 = (By.ID, "id_registration-password1")
+    REGISTER_FORM_PSW_IMPUT_2 = (By.ID, "id_registration-password2")
+    REGISTER_SUBMIT = (By.CSS_SELECTOR, "#register_form .btn-primary")
+
+
+class ProfilePageLocators:
+    URL = "http://selenium1py.pythonanywhere.com/ru/accounts/profile/"
+    PAGE_HEADER = (By.CLASS_NAME, "page-header")
+    START_DELETE_PROFILE_SUBMIT = (By.ID, "delete_profile")
+    DELETE_PROFILE_PASSWORD_INPUT = (By.ID, "id_password")
+    DELETE_PROFILE_SUBMIT = (By.CLASS_NAME, "btn-danger")
 
 
 class ProductPageLocators:
