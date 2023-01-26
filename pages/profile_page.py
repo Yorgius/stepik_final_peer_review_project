7c01256fd0a54118d8ef80e43ea3d69629eb26b2
Dmitrij Manin <manin.dmitrij@gmail.com>
2023-01-26 08:17:15 +0300

    added profile page for delete methods of user

---
+++ pages/profile_page.py
@@ -0,0 +1,36 @@
+from .base_page import BasePage
+from .locators import ProfilePageLocators
+
+class ProfilePage(BasePage):
+    def should_be_profile_page(self):
+        self.should_be_profile_url()
+        self.should_be_profile_page_header()
+
+    def should_be_profile_url(self):
+        assert self.browser.current_url == ProfilePageLocators.URL
+
+    def should_be_profile_page_header(self):
+        assert self.is_element_present(*ProfilePageLocators.PAGE_HEADER)
+
+    def start_delete_user(self):
+        delete_profile_submit = self.browser.find_element(*ProfilePageLocators.START_DELETE_PROFILE_SUBMIT)
+        delete_profile_submit.click()
+
+
+class DeleteProfilePage(BasePage):
+    def should_be_delete_profile_page(self):
+        self.should_be_delete_password_input()
+        self.should_be_delete_profile_submit()
+    
+    def should_be_delete_password_input(self):
+        assert self.is_element_present(*ProfilePageLocators.DELETE_PROFILE_PASSWORD_INPUT)
+
+    def should_be_delete_profile_submit(self):
+        assert self.is_element_present(*ProfilePageLocators.DELETE_PROFILE_SUBMIT)
+
+    def delete_user(self, password):
+        password_input = self.browser.find_element(*ProfilePageLocators.DELETE_PROFILE_PASSWORD_INPUT)
+        password_input.send_keys(password)
+        
+        delete_submit = self.browser.find_element(*ProfilePageLocators.DELETE_PROFILE_SUBMIT)
+        delete_submit.click()
